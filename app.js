2650c774dc177cdb248e01ba97da61ed4e1bb6f2
Marcos <marcoscastillo@correo.ugr.es>
2020-10-21 11:18:11 +0200

    AdminBro Panel

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 require('./database');
@@ -27,6 +28,7 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
