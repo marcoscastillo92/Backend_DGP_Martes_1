cc37765f7ef361f44d19991679b6474048b33d3e
Marcos <marcoscastillo@correo.ugr.es>
2020-10-28 09:13:25 +0100

    Authentication by token
    - Se cambia la forma de autenticarse para que sea mediante token en la petición, ya sea get o post.
    - Se corrige la generación del token en el schema de User.
    - Se ponen como campos con valores únicos el username y token.
    - Se modifica la ruta localhost:3000 para que muestre "API Asociación Vale" y no cree usuarios vacios.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,19 @@
-var auth = function(req, res, next) {
-    if (req.session && req.session.user)
+var User = require('../models/User');
+
+var auth = async function(req, res, next) {
+  var token;
+  if(req.body)
+    token = req.body.token;
+  else
+    token = req.query.token;
+
+  if (token && token != ""){
+    var user = await User.findOne({ token: req.body.token });
+    if (user && user.username != ""){
       return next();
-    else
-      return res.sendStatus(401);
-  };
+    }
+  }
+  return res.sendStatus(401);
+};
 
 module.exports = auth;
